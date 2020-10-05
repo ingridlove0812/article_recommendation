5da164113443ff43760a10b845173788a3431a2f
ingridlove0812 <49986298+ingridlove0812@users.noreply.github.com>
2020-10-05 17:46:59 +0800

    update

--- recommendation_demo/code/mart/db_connect.py
+++ recommendation_demo/code/mart/db_connect.py
@@ -5,9 +5,9 @@ import mysql.connector
 
 def connect_sql():
     conn = mysql.connector.connect(
-            user='crm360',
-            password='crm360_as!Wsd',
-            host='crm360-db.cljcsr7rrceb.ap-southeast-2.rds.amazonaws.com',
+            user=user,
+            password=password,
+            host=host,
             port=3306,
             charset='utf8mb4',
             database='tvbs_360')
@@ -16,20 +16,18 @@ def connect_sql():
 
 def connect_sql_crm():
     conn = mysql.connector.connect(
-            user='tvbs',
-            password='tvbs_qzDw2Ae34',
-            host='tvbs-0612.cljcsr7rrceb.ap-southeast-2.rds.amazonaws.com',
+            user=user,
+            password=password,
+            host=host,
             port=3306,
             database='tvbs')
     cur = conn.cursor()
     return conn, cur
 
 def connect_mongo():
-    host = ['cluster0-shard-00-00-bnmv8.mongodb.net:27017',
-            'cluster0-shard-00-01-bnmv8.mongodb.net:27017',
-            'cluster0-shard-00-02-bnmv8.mongodb.net:27017']
-    user = 'tvbs360'
-    passwd = 'Tvbs360Pssw0rd'
+    host = host
+    user = user
+    passwd = password
     client = pymongo.MongoClient(host,
                           username=user,
 						 password=passwd,
@@ -39,11 +37,9 @@ def connect_mongo():
     return client
 
 def connect_mongo_dev():
-    host = ['cluster360-dev-shard-00-00-bnmv8.mongodb.net:27017',
-            'cluster360-dev-shard-00-01-bnmv8.mongodb.net:27017',
-            'cluster360-dev-shard-00-02-bnmv8.mongodb.net:27017']
-    user = 'tvbs360'
-    passwd = 'Tvbs360Pssw0rd'
+    host = host
+    user = user
+    passwd = password
     client = pymongo.MongoClient(host,
                           username=user,
 						 password=passwd,
@@ -53,9 +49,9 @@ def connect_mongo_dev():
 
 def connect_sql_aws(database = 'tvbs_v4'):
     conn = mysql.connector.connect(
-            user='tvbs',
-            password='tvbstvbs',
-            host='db41-ro-tvbs-aurora.clwuef820xta.ap-northeast-1.rds.amazonaws.com',
+            user=user,
+            password=password,
+            host=host,
             port=3306,
             charset='utf8mb4',
             database = database)
@@ -64,9 +60,9 @@ def connect_sql_aws(database = 'tvbs_v4'):
 
 def connect_sql_gcp(database = 'WarehouseServer'):
     conn = mysql.connector.connect(
-            user='lailai',
-            password='dsTVBS84305300tvbs',
-            host='10.33.0.3',
+            user=user,
+            password=password,
+            host=host,
             port=3306,
             database = database ,
             charset='utf8mb4'
