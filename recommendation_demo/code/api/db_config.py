c972d57f76823967b4c10eb4c745c78409cea9ca
ingridlove0812 <49986298+ingridlove0812@users.noreply.github.com>
2021-07-30 10:15:22 +0800

    Update db_config.py

--- recommendation_demo/code/api/db_config.py
+++ recommendation_demo/code/api/db_config.py
@@ -1,10 +1,27 @@
-from app import app
 from flaskext.mysql import MySQL
+from flask import Flask
+import json
+
+with open(f"C:\\Users\\Public\\Documents\\db_config\\db_info.json") as json_file: 
+    db_info = json.load(json_file) 
+ 
+
+app = Flask(__name__)
+
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'MartServer'
-app.config['MYSQL_DATABASE_HOST'] = 'xxx.xxx.xxx.xxx'
-mysql.init_app(app)
+def gcp_db():
+    app.config['MYSQL_DATABASE_USER'] = db_info['gcp']['username']
+    app.config['MYSQL_DATABASE_PASSWORD'] = db_info['gcp']['password']
+    app.config['MYSQL_DATABASE_DB'] = db_info['gcp']['db']
+    app.config['MYSQL_DATABASE_HOST'] = db_info['gcp']['host']
+    mysql.init_app(app)
+    
+
+def aws_db():
+    app.config['MYSQL_DATABASE_USER'] = db_info['aws']['username']
+    app.config['MYSQL_DATABASE_PASSWORD'] = db_info['aws']['password']
+    app.config['MYSQL_DATABASE_DB'] = db_info['aws']['db']
+    app.config['MYSQL_DATABASE_HOST'] = db_info['aws']['host']
+    mysql.init_app(app)
